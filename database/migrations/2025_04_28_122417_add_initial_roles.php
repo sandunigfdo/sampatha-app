ba75960e42946b09a89ad72eac9b4b05f5448aed
Sanduni Fernando <sandunigfdo@gmail.com>
2025-04-30 15:49:26 +1200

    Insert initialk records to roles table

---
+++ database/migrations/2025_04_28_122417_add_initial_roles.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('roles')->insert([
+            'id' => 1,
+            'role_name' => 'admin',
+        ]);
+        DB::table('roles')->insert([
+            'id' => 2,
+            'role_name' => 'user',
+        ]);
+        DB::table('roles')->insert([
+            'id' => 3,
+            'role_name' => 'manager',
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};

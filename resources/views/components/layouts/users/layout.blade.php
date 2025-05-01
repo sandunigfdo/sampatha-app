c67fe7bda52eba940498faaff8fe51155da78880
Sanduni Fernando <sandunigfdo@gmail.com>
2025-05-01 17:06:54 +1200

    Create a seperate layout for users

---
+++ resources/views/components/layouts/users/layout.blade.php
@@ -0,0 +1,7 @@
+<div class="flex items-start max-md:flex-col">       
+    <div class="flex-1 self-stretch max-md:pt-6">
+        <div class="mt-5 w-full max-w-lg">            
+            {{ $slot }}            
+        </div>
+    </div>
+</div>

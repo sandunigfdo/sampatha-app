94b6ada3604602add9533f3ad4bc087dec7c609b
Sanduni Fernando <sandunigfdo@gmail.com>
2025-05-05 23:22:58 +1200

    Create roles

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,13 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Role extends Model
 {
+    use HasFactory;
+
+    public const IS_ADMIN = 1;
+    public const IS_USER = 2;
+    public const IS_MANAGER = 3;
+
     /**
-     * Get the users associated with the roles.
+     * Relationship with user.
      */
     public function users():HasMany
     {

5cb53b816b94100d90da9b0bc374722913d4a672
Sanduni Fernando <sandunigfdo@gmail.com>
2025-05-05 23:24:08 +1200

    Create roles

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Role;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -29,7 +30,28 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'role_id'=> Role::IS_USER,
         ];
+    }
+
+    /**
+     * Indicate that the user is admin.
+     */
+    public function asAdmin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role_id' => Role::IS_ADMIN,
+        ]);
+    }
+
+    /**
+     * Indicate that the user is a manager
+     */
+    public function asManager(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role_id' => Role::IS_MANAGER,
+        ]);
     }
 
     /**
@@ -39,6 +61,16 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    /**
+     * Indicate that the user clicked "Remember me"
+     */
+    public function rememberTrue(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'remember_token' => Str::random(10),
         ]);
     }
 }

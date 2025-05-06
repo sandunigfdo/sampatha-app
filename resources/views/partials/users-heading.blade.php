7c1e00dc40b368368e21b4d80a94e065be9bfc89
Sanduni Fernando <sandunigfdo@gmail.com>
2025-05-07 10:51:53 +1200

    new heading for the user management

---
+++ resources/views/partials/users-heading.blade.php
@@ -0,0 +1,5 @@
+<div class="relative mb-6 w-full">
+    <flux:heading size="xl" level="1">{{ __('Users') }}</flux:heading>
+    <flux:subheading size="lg" class="mb-6">{{ __('Add new users or make changes to existing user accounts') }}</flux:subheading>
+    <flux:separator variant="subtle" />
+</div>

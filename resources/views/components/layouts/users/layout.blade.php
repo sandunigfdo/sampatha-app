a83f7b493f94626c15e86feba59ca82e86d30432
Sanduni Fernando <sandunigfdo@gmail.com>
2025-05-07 10:47:20 +1200

    Change the layout of the page to have left and right sections

--- resources/views/components/layouts/users/layout.blade.php
+++ resources/views/components/layouts/users/layout.blade.php
@@ -1,7 +1,45 @@
-<div class="flex items-start max-md:flex-col">       
+{{-- <div class="flex items-start max-md:flex-col">       
     <div class="flex-1 self-stretch max-md:pt-6">
         <div class="mt-5 w-full max-w-lg">            
             {{ $slot }}            
         </div>
     </div>
+</div> --}}
+
+
+<div class="flex items-start max-md:flex-col">
+    <div class="me-10 w-full pb-4 md:w-[220px]">
+        <flux:navlist>            
+            <flux:navlist.item :href="route('list_users')" wire:navigate>{{ __('List Users') }}</flux:navlist.item>                  
+        </flux:navlist>
+    </div>
+
+    <flux:separator class="md:hidden" />
+
+    {{-- <div class="flex-1 self-stretch max-md:pt-6">
+        <flux:heading>{{ $heading ?? '' }}</flux:heading>
+        <flux:subheading>{{ $subheading ?? '' }}</flux:subheading>
+
+        <div class="mt-5 w-full max-w-lg">
+            {{ $slot }}
+        </div>       
+
+    </div> --}}
+
+    <div class="flex flex-col md:flex-row gap-6 flex-1 self-stretch max-md:pt-6">
+        <div class="w-full md:w-1/2"> 
+            <flux:heading>{{ $heading ?? '' }}</flux:heading>
+            <flux:subheading>{{ $subheading ?? '' }}</flux:subheading>
+
+            <div class="mt-5 w-full">
+                {{ $slot }}
+            </div>
+        </div>
+        <div class="w-full md:w-1/4 self-stretch">
+            {{ $right ?? '' }}
+        </div>
+    </div>
+
+    
 </div>
+

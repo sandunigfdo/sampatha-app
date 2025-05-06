bd9fb32d6f9a067305abac34408451b3805ff861
Sanduni Fernando <sandunigfdo@gmail.com>
2025-05-07 10:51:17 +1200

    Change the name of the heading file to a comman name

--- resources/views/partials/create-user-heading.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="relative mb-6 w-full">
-    <flux:heading size="xl" level="1">{{ __('Add User') }}</flux:heading>
-    <flux:subheading size="lg" class="mb-6">{{ __('Add a new user to the system') }}</flux:subheading>
-    <flux:separator variant="subtle" />
-</div>

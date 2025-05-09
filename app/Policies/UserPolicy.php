0c513437bd470df5e058258252646d7f60f2acd6
Sanduni Fernando <sandunigfdo@gmail.com>
2025-05-09 13:12:49 +1200

    Add a policy filter for authorize actions

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -8,6 +8,23 @@ use Illuminate\Auth\Access\Response;
 class UserPolicy
 {
     /**
+     * Perform pre-authorization checks.
+     * Admin and manager user can create, update or delete user.
+     *
+     * @param User $user
+     * @return boolean
+     */
+    public function before(User $user, string $ability): bool|null
+    {
+        if ($user->role_id ===1 || $user->role_id ===3){
+            return true;
+        }
+
+        return null;
+    }
+
+
+    /**
      * Determine whether the user can view any models.
      */
     public function viewAny(User $user): bool
@@ -25,23 +42,25 @@ class UserPolicy
 
     /**
      * Determine whether the user can create models.
-     * Admin and manager can create a new user.
+     * 
      */
     public function create(User $user): bool
     {
-        return $user->role_id === 1 || $user->role_id === 3;
+        return false;
     }
 
     /**
      * Determine whether the user can update the model.
+     * 
      */
-    public function update(User $user, User $model): bool
+    public function update(User $user): bool
     {
         return false;
     }
 
     /**
      * Determine whether the user can delete the model.
+     * 
      */
     public function delete(User $user, User $model): bool
     {

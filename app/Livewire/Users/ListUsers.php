8de92d42e5f8630d9d4eae2a802d8a391d6220f0
Sanduni Fernando <sandunigfdo@gmail.com>
2025-05-09 13:11:37 +1200

    Add a method to delete a selected user

--- app/Livewire/Users/ListUsers.php
+++ app/Livewire/Users/ListUsers.php
@@ -2,10 +2,30 @@
 
 namespace App\Livewire\Users;
 
+use App\Models\User;
 use Livewire\Component;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ListUsers extends Component
 {
+      /**
+     * Delete the selected user
+     */ 
+    public function delete_user($id, User $user): void
+    {
+       if(Auth::user()->cannot('delete', $user)) {
+            abort(403);
+       }
+
+        // Delete the selected user
+
+        $user = User::findOrFail($id);                     
+        $user->delete();
+        
+        // #TODO check whether the user has permission to delete a user
+    } 
+
     public function render()
     {
         return view('livewire.users.list-users');

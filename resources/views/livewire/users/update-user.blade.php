6801562d20fb59024ef28e8c509ebb17a0a21eb0
Sanduni Fernando <sandunigfdo@gmail.com>
2025-05-09 13:17:46 +1200

    Add a flux callout to display the success message

--- resources/views/livewire/users/update-user.blade.php
+++ resources/views/livewire/users/update-user.blade.php
@@ -8,7 +8,7 @@
             <div>
                 <flux:input wire:model="email" :label="__('Email')" type="email" required autocomplete="email" />
 
-                @if (auth()->user() instanceof \Illuminate\Contracts\Auth\MustVerifyEmail &&! auth()->user()->hasVerifiedEmail())
+                @if (auth()->user() instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && !auth()->user()->hasVerifiedEmail())
                     <div>
                         <flux:text class="mt-4">
                             {{ __('Your email address is unverified.') }}
@@ -37,56 +37,52 @@
                 </x-action-message>
             </div>
         </form> --}}
-        
-                    
+
+
         <!-- Session Status -->
         <x-auth-session-status class="text-center" :status="session('status')" />
-    
+
         <form wire:submit="update_user" class="my-6 w-full space-y-6">
             <!-- Name -->
-            <flux:input
-                wire:model="name"
-                :label="__('Name')"
-                type="text"
-                required
-                autofocus
-                autocomplete="name"                
-                :placeholder="__('Full name')"
-            />
-    
-            <!-- Email Address -->
-            <flux:input
-                wire:model="email"
-                :label="__('Email address')"
-                type="email"
-                required
-                autocomplete="email"
-                placeholder="email@example.com"
-            />               
+            <flux:input wire:model="name" :label="__('Name')" type="text" required autofocus autocomplete="name"
+                :placeholder="__('Full name')" />
 
-            <!-- Role -->                
-            <flux:select 
-            wire:model="role"                                        
-            placeholder="Select role"
-            :label="__('User Role')"
-            >
-                @foreach (\App\Models\Role::all() as $role )
-                    <flux:select.option value="{{ $role->id }}">{{ $role->role_name }}</flux:select.option>                    
+            <!-- Email Address -->
+            <flux:input wire:model="email" :label="__('Email address')" type="email" required autocomplete="email"
+                placeholder="email@example.com" />
+
+            <!-- Role -->
+            <flux:select wire:model="role" placeholder="Select role" :label="__('User Role')">
+                @foreach (\App\Models\Role::all() as $role)
+                    <flux:select.option value="{{ $role->id }}">{{ $role->role_name }}</flux:select.option>
                 @endforeach
-                                    
-            </flux:select>               
-                                    
+
+            </flux:select>
+
             <div class="flex items-center gap-4">
-                <div class="flex items-center justify-end gap-6">
-                    <flux:button variant="primary" type="submit" class="w-full">{{ __('Save') }}</flux:button>
-                    <flux:button href="{{ route('list_users') }}" variant="filled" type="submit" class="w-full">{{ __('Cancel') }}</flux:button>
+                <div class="flex items-center justify-end space-x-6">
+                    <flux:button variant="primary" type="submit" class="w-full" :loading="true">
+                        {{ __('Save') }}</flux:button>
+
+                    <flux:modal.close>
+                        <flux:button href="{{ route('list_users') }}" variant="filled" type="submit" class="w-full">
+                            {{ __('Cancel') }}</flux:button>
+                    </flux:modal.close>
                 </div>
-               
-                <x-action-message class="me-3" on="user-updated">
-                    {{ __('Saved') }}
-                </x-action-message>
+
+                {{-- <x-action-message class="me-3" on="user-updated">                   
+                    <flux:callout variant="success" icon="check-circle" heading="Saved." />
+                </x-action-message> --}}
+
             </div>
-                            
-        </form>    
+            <div class="flex flex-col md:flex-row flex-1 max-md:pt-6">
+                <div class="w-1/5">
+                    <x-action-message class="me-3" on="user-updated">
+                        <flux:callout variant="success" icon="check-circle" heading="Saved" />                        
+                    </x-action-message>
+                </div>
+            </div>
+
+        </form>
     </x-layouts.users.layout>
 </section>

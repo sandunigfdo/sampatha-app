59d90c2dffbbc43e5129cf5563c867e5ac17f6ff
Sanduni Fernando <sandunigfdo@gmail.com>
2025-05-11 23:21:46 +1200

    Change the route list users

--- resources/views/components/layouts/users/layout.blade.php
+++ resources/views/components/layouts/users/layout.blade.php
@@ -10,7 +10,7 @@
 <div class="flex items-start max-md:flex-col">
     <div class="me-10 w-full pb-4 md:w-[220px]">
         <flux:navlist>            
-            <flux:navlist.item :href="route('list_users')" wire:navigate>{{ __('List Users') }}</flux:navlist.item>                  
+            <flux:navlist.item :href="route('users.list')" wire:navigate>{{ __('List Users') }}</flux:navlist.item>                  
         </flux:navlist>
     </div>
 

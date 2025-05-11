008b4381193915afda6e93f261a171e6765b8422
Sanduni Fernando <sandunigfdo@gmail.com>
2025-05-12 10:58:51 +1200

    Make the width to 7xl

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,7 @@
 <x-layouts.app.header :title="$title ?? null">
     <flux:main>
-        {{ $slot }}
+        <div class="mx-auto max-w-7xl">
+            {{ $slot }}
+        </div>
     </flux:main>
 </x-layouts.app.header>

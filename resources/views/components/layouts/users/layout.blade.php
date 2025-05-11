e7a66fb715fbbd456ad911776f5b7479176e7676
Sanduni Fernando <sandunigfdo@gmail.com>
2025-05-11 23:22:50 +1200

    Remove the side navigation and right section

--- resources/views/components/layouts/users/layout.blade.php
+++ resources/views/components/layouts/users/layout.blade.php
@@ -8,15 +8,15 @@
 
 
 <div class="flex items-start max-md:flex-col">
-    <div class="me-10 w-full pb-4 md:w-[220px]">
+    {{-- <div class="me-10 w-full pb-4 md:w-[220px]">
         <flux:navlist>            
             <flux:navlist.item :href="route('users.list')" wire:navigate>{{ __('List Users') }}</flux:navlist.item>                  
         </flux:navlist>
-    </div>
+    </div> --}}
 
     <flux:separator class="md:hidden" />
 
-    {{-- <div class="flex-1 self-stretch max-md:pt-6">
+    <div class="flex-1 self-stretch max-md:pt-6">
         <flux:heading>{{ $heading ?? '' }}</flux:heading>
         <flux:subheading>{{ $subheading ?? '' }}</flux:subheading>
 
@@ -24,9 +24,9 @@
             {{ $slot }}
         </div>       
 
-    </div> --}}
+    </div>
 
-    <div class="flex flex-col md:flex-row gap-6 flex-1 self-stretch max-md:pt-6">
+    {{-- <div class="flex flex-col md:flex-row gap-6 flex-1 self-stretch max-md:pt-6">
         <div class="w-full md:w-1/2"> 
             <flux:heading>{{ $heading ?? '' }}</flux:heading>
             <flux:subheading>{{ $subheading ?? '' }}</flux:subheading>
@@ -38,7 +38,7 @@
         <div class="w-full md:w-1/4 self-stretch">
             {{ $right ?? '' }}
         </div>
-    </div>
+    </div> --}}
 
     
 </div>

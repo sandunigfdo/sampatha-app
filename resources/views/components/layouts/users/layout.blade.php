9aec6ac4797031bc8ca6a3efe9556a9d2c551fb4
Sanduni Fernando <sandunigfdo@gmail.com>
2025-05-12 14:22:40 +1200

    Removed the butoon and update the width of contrnt to max-large

--- resources/views/components/layouts/users/layout.blade.php
+++ resources/views/components/layouts/users/layout.blade.php
@@ -1,27 +1,11 @@
 <div class="flex items-start max-md:flex-col">
-    {{-- <div class="me-10 w-full pb-4 md:w-[220px]">
-        <flux:navlist>            
-            <flux:navlist.item :href="route('users.list')" wire:navigate>{{ __('List Users') }}</flux:navlist.item>                  
-        </flux:navlist>
-    </div> --}}
-
     <flux:separator class="md:hidden" />
 
     <div class="flex-1 self-stretch max-md:pt-6">
-        <div class="flex mx-auto items-center justify-between">
-            <div>
-                <flux:heading>{{ $heading ?? '' }}</flux:heading>
-                <flux:subheading>{{ $subheading ?? '' }}</flux:subheading>
-            </div>
-            <div>
-            
-                <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                    <flux:button href="{{ route('users.create') }}" icon:trailing="user-plus">Add User</flux:button>
-                </div>
-            </div>
-        </div>
+        <flux:heading>{{ $heading ?? '' }}</flux:heading>
+        <flux:subheading>{{ $subheading ?? '' }}</flux:subheading> 
 
-        <div class="mt-5 w-full">
+       <div class="mt-5 w-full max-w-lg">
             {{ $slot }}
         </div>       
 

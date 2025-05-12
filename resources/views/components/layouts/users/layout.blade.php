d434dcab5b7b6cf59035c440ba68d01f2449690c
Sanduni Fernando <sandunigfdo@gmail.com>
2025-05-12 13:33:44 +1200

    Cleanup

--- resources/views/components/layouts/users/layout.blade.php
+++ resources/views/components/layouts/users/layout.blade.php
@@ -1,12 +1,3 @@
-{{-- <div class="flex items-start max-md:flex-col">       
-    <div class="flex-1 self-stretch max-md:pt-6">
-        <div class="mt-5 w-full max-w-lg">            
-            {{ $slot }}            
-        </div>
-    </div>
-</div> --}}
-
-
 <div class="flex items-start max-md:flex-col">
     {{-- <div class="me-10 w-full pb-4 md:w-[220px]">
         <flux:navlist>            
@@ -34,22 +25,6 @@
             {{ $slot }}
         </div>       
 
-    </div>
-
-    {{-- <div class="flex flex-col md:flex-row gap-6 flex-1 self-stretch max-md:pt-6">
-        <div class="w-full md:w-1/2"> 
-            <flux:heading>{{ $heading ?? '' }}</flux:heading>
-            <flux:subheading>{{ $subheading ?? '' }}</flux:subheading>
-
-            <div class="mt-5 w-full">
-                {{ $slot }}
-            </div>
-        </div>
-        <div class="w-full md:w-1/4 self-stretch">
-            {{ $right ?? '' }}
-        </div>
-    </div> --}}
-
-    
+    </div>    
 </div>
 

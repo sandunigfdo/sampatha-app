a36e5ffbdbaf8459023d6edd4e32b448fa35321f
Sanduni Fernando <sandunigfdo@gmail.com>
2025-05-14 17:43:34 +1200

    Clean up

--- resources/views/livewire/users/update-user.blade.php
+++ resources/views/livewire/users/update-user.blade.php
@@ -2,43 +2,7 @@
     @include('partials.users-heading')
 
     <x-layouts.users.layout :heading="__('Update user')" :subheading="__('Update a user account.')">
-        {{-- <form wire:submit="updateProfileInformation" class="my-6 w-full space-y-6">
-            <flux:input wire:model="name" :label="__('Name')" type="text" required autofocus autocomplete="name" />
-
-            <div>
-                <flux:input wire:model="email" :label="__('Email')" type="email" required autocomplete="email" />
-
-                @if (auth()->user() instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && !auth()->user()->hasVerifiedEmail())
-                    <div>
-                        <flux:text class="mt-4">
-                            {{ __('Your email address is unverified.') }}
-
-                            <flux:link class="text-sm cursor-pointer" wire:click.prevent="resendVerificationNotification">
-                                {{ __('Click here to re-send the verification email.') }}
-                            </flux:link>
-                        </flux:text>
-
-                        @if (session('status') === 'verification-link-sent')
-                            <flux:text class="mt-2 font-medium !dark:text-green-400 !text-green-600">
-                                {{ __('A new verification link has been sent to your email address.') }}
-                            </flux:text>
-                        @endif
-                    </div>
-                @endif
-            </div>
-
-            <div class="flex items-center gap-4">
-                <div class="flex items-center justify-end">
-                    <flux:button variant="primary" type="submit" class="w-full">{{ __('Save') }}</flux:button>
-                </div>
-
-                <x-action-message class="me-3" on="profile-updated">
-                    {{ __('Saved.') }}
-                </x-action-message>
-            </div>
-        </form> --}}
-
-
+        
         <!-- Session Status -->
         <x-auth-session-status class="text-center" :status="session('status')" />
 
